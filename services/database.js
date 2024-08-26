148d4ae5871fc4ea6da6aa80ba1f93aae4c2430e
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-08-26 20:42:35 +0530

    Node js postgresql setup

---
+++ services/database.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+});
+
+module.exports = {
+  pool,
+};
