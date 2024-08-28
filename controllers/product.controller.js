15f1e8c6537da7a039d401285a68ffd7742d9980
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-08-28 16:55:54 +0530

    add product by id and category filter api routes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -120,3 +120,41 @@ exports.deleteProduct = async (req, res) => {
     return res.status(500).json(error);
   }
 };
+
+exports.getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query({
+      text: `SELECT * FROM product WHERE id = $1`,
+      values: [id],
+    });
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Product not exist" });
+    }
+
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+exports.getProductByCategory = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query({
+      text: `SELECT * FROM product WHERE category_id = $1`,
+      values: [id],
+    });
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Category not exist" });
+    }
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
--- routes/product.route.js
+++ routes/product.route.js
@@ -5,9 +5,13 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  getProductById,
+  getProductByCategory,
 } = require("../controllers/product.controller.js");
 
 router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.get("/category/:id", getProductByCategory);
 router.post("/create-product", createProduct);
 router.put("/update-product/:id", updateProduct);
 router.delete("/delete-product/:id", deleteProduct);
